f7a716b02fc3fd0b4e2551ab35e925779e3cf937
kjfreidhof <89760217+kjfreidhof@users.noreply.github.com>
2022-05-06 20:52:58 -0400

    Add files via upload

--- user.sh
+++ user.sh
@@ -35,8 +35,61 @@ cp .bashrc.bak ~/"$USERNAME"
 # this makes a .icons directory and .themes directory 
 # in my home directory
 mkdir -p /home/"$USERNAME"/.icons
-
 mkdir -p /home/"$USERNAME"/.themes 
 
+# This here extracts each icon them and copies them to the .icons directory in my home directory 
+
+tar -xvf 01-McMojave-circle.tar.xz
+
+cp -r McMojave-circle ~/.icons
+cp -r McMojave-circle-dark ~/,icons 
+
+tar -xvf candy-icons.tar.xz
+
+cp -r candy-icons ~/.icons
+
+unzip Dracula.zip 
+
+cp -r Dracula ~/.icons 
+
+tar -xvf Mkos-Big-Sur.tar.xz 
+
+cp -r Mkos-Big-Sur ~/.icons 
+
+cp -r Mkos-Big-Sur-Night ~/.icons 
+
+cp -r Mkos-Big-Sur-Panel-white ~/.icons
+
+tar -xvf  papirus-icon-theme-20220302.tar.gz
+
+cp -r Papirus ~/.icons
+
+cp -r Papirus-Dark ~/.icons
+
+cp -r Papirus-light ~/.icons 
+
+# This here extracts and copies my themes over to my .themes directory in my home directory 
+
+unzip gtk-master.zip
+
+cp -r gtk-master ~/.themes 
+
+unzip Gruvbox-Dark.zip 
+
+cp -r Gruvbox-Dark ~/.themes
+
+tar -xvf Mojave-dark.tar.xz
+
+cp -r Mojave-dark ~/.themes 
+
+unzip Ubuntu-Monterey.zip
+
+cp -r Ubuntu-Monterey ~/.themes 
+
+tar -xvf Sweet.tar.xz
+
+cp -r Sweet ~/.themes 
 
 
+
+exit 
